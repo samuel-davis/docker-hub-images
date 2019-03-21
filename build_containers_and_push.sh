84777c2e4193f7e0fb941dd007a329d4b5696f42
Sam Davis <sam.davis@techngs.com>
2019-03-21 13:04:09 -0400

    mi

--- build_containers_and_push.sh
+++ build_containers_and_push.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 docker-compose -f compose.yml build
 #docker login --username=samdavis2
-docker push samdavis2/docker-compose-java:2
+docker push samdavis2/docker-compose-java:2.1
+#docker push samdavis2/docker-compose-java-ubuntu:2.1
 #docker push samdavis2/nifi-no-volumes:1.8.0
