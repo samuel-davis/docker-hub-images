bd03fba1ce165e87c77311fd3ba6e3d207724aba
Sam Davis <sam.davis@techngs.com>
2019-08-09 10:12:29 -0400

    Fixed docker no volumes.

--- build_containers_and_push.sh
+++ build_containers_and_push.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 export NIFI_VERSION=1.8.0
-export COMPOSE_BUILDER_VERSION=2.4
+export COMPOSE_BUILDER_VERSION=2.6
 export NPM_BUILDER_VERSION=1.3
 
 
 docker-compose -f compose.yml build
 #docker login --username=samdavis2
 docker push samdavis2/docker-compose-java:$COMPOSE_BUILDER_VERSION
+docker tag samdavis2/docker-compose-java-di2e:$COMPOSE_BUILDER_VERSION docker-di2e.di2e.net/dlvp/docker-compose-java:$COMPOSE_BUILDER_VERSION
+docker push docker-di2e.di2e.net/dlvp/docker-compose-java:$COMPOSE_BUILDER_VERSION
 #docker push samdavis2/npm-builder:$NPM_BUILDER_VERSION
 #docker push samdavis2/nifi-no-volumes:$NIFI_VERSION
